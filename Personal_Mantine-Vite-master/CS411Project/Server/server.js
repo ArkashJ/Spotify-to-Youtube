d5665239c2f7b9a8b5750811a362b9fa572a51d3
Arkash Jain <arkjain@bu.edu>
2022-10-30 09:48:10 -0400

    connection to the database

--- Personal_Mantine-Vite-master/CS411Project/Server/server.js
+++ Personal_Mantine-Vite-master/CS411Project/Server/server.js
@@ -1,15 +1,16 @@
-const express = require('express')
+const express = require("express");
+const cors = require("cors");
 const app = express()
-const port = 3000
+const connection = require("./db")
 
-app.get('/', (req,res) =>{
-    res.send('Hello World')
+require("dotenv").config()
+const port = process.env.PORT || 8080
+connection()
+
+app.get("/", (req, res) => {
+    res.send("hello world")
 })
-
-app.listen(port, () =>{
-    console.log(`example app listening on port ${port}`)
-})
-
-app.use(express.static('public'))
+app.listen(port, () => console.log(`Listening on port ${port}`))
 
 // use absolute path for other directories
+// app.use('/static', express.static(path.join(__dirname, 'public')))
