b1426ca68aadb78cdd538d3b85d111e3b34f2aa2
Arkash Jain <arkjain@bu.edu>
2022-11-03 19:56:56 -0400

    set up client id and secret, getting the passport oauth ready

--- Personal_Mantine-Vite-master/CS411Project/Server/models/user.js
+++ Personal_Mantine-Vite-master/CS411Project/Server/models/user.js
@@ -3,7 +3,6 @@ const jwt = require("jsonwebtoken");
 const PassJ = require("joi")
 const passwordComplexity = require("joi-password-complexity");
 
- 
 const userSchema = new mongoose.Schema({
     firstName   :   {type:String, required: true},
     lastName    :   {type:String, required: true},
--- Personal_Mantine-Vite-master/CS411Project/Server/passportOAuth.js
+++ Personal_Mantine-Vite-master/CS411Project/Server/passportOAuth.js
@@ -3,13 +3,21 @@ const { User } = require('./models/user')
 const GoogleStrategy = require("passport-google-ouath20").Strategy
 
 passport.use(new GoogleStrategy({
-        clientID: "",
-        clientSecret: "",
+        clientID: process.env.GOOGLECLIENTID,
+        clientSecret: process.env.GOOGLECLIENTSERCRET,
         callbackURL: "http://localhost:8080/auth/google/callback"
     },
     function(accessToken, refreshToken, profile, cb){
         User.findOrCreate({googleId: profile.id}, function(err,user){
-            return cb(err, user)
+            return cb(err, user);
         })
     }
-    ))
+))
+
+passport.serializeUser((user,done) =>{
+    done(null, user.id)
+});
+
+passport.deserializeUser((user, done) => {
+    done(null, user.id)
+})
