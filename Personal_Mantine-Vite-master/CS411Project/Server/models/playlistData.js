57f7446ebe9e57fd7e508ce8a466ef8de572212d
Arkash Jain <arkjain@bu.edu>
2022-11-11 09:54:25 -0500

    code to authorize the user, refresh tokens, and access and store the tracks of a playlist in files getMyPlaylist and getData

--- Personal_Mantine-Vite-master/CS411Project/Server/models/playlistData.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require("mongoose");
-
-const playlistSchema = new Schema({
-    name: {type: String, required: true}
-})
--- Personal_Mantine-Vite-master/CS411Project/Server/routes/routes.js
+++ Personal_Mantine-Vite-master/CS411Project/Server/routes/routes.js
@@ -5,6 +5,9 @@ const Model     = require("../models/playlistData")
 router.get('/getPlaylist', async(req, res) => {
     try{
         const data = Model.find()
+        res.json(data)
+    } catch(error){
+        res.status(500).json({message: error.message})
     }
 })
 
