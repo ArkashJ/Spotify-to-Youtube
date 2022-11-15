158bcd61a814c55c61d1adb63ea0dc52dc925e0a
Arkash Jain <arkjain@bu.edu>
2022-11-15 18:32:33 -0500

    made a table to display songs

--- Personal_Mantine-Vite-master/CS411Project/client/vite-project/src/components/MainPage/DisplaySongs.jsx
+++ Personal_Mantine-Vite-master/CS411Project/client/vite-project/src/components/MainPage/DisplaySongs.jsx
@@ -1,7 +1,16 @@
 import axios from "axios";
 import React from "react";
+import {DataGrid} from '@mui/x-data-grid';
 
 const baseURL = "http://localhost:8081/api/getAll";
+
+const columns = [
+  { field: 'id',        headerName: 'ID',         width: 70 },
+  { field: 'songName',  headerName: 'Song Name',  width: 230},
+  { field: 'artist',    headerName: 'Artist',     width: 130},
+  { field: 'albumName', headerName: 'Album Name', width: 230},
+  { field: 'duration',  headerName: 'Duration',   width: 100, type: 'number'}
+]
 
 export default function DisplaySongs() {
   const [post, setPost] = React.useState(null);
@@ -13,25 +22,29 @@ export default function DisplaySongs() {
   }, []);
 
   if (!post) return null;
-    let data = post.map((item) => {
-        return (
-            <tr key = {item.id}>
-                <td>{item.duration}</td>
-                <td>{item.songName}</td>
-                <td>{item.albumName}</td>
-                <td>{item.artist}</td>
-                <td>{item.albumId}</td>
-            </tr>
-        )
+
+  let rows = [];
+
+    post.map((item) => {
+        rows.push(
+              {
+                id        : item.albumId,
+                duration  : item.duration,
+                songName  : item.songName,
+                albumName : item.albumName,
+                artist    : item.artist
+              })
     })
+
   return (
-    
-    <div>
-        <table>
-            <tbody>
-                {data}
-            </tbody>
-        </table>
+    <div style={{ height: 800, width: '100%' }}>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        pageSize={rows.length}
+        rowsPerPageOptions={[10]}
+        checkboxSelection
+      />
     </div>
   );
 }
