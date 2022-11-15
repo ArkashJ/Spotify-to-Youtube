4519c3f8bd2669083aa5c674eafc58c8efc7f604
Arkash Jain <arkjain@bu.edu>
2022-11-14 23:16:11 -0500

    making router methods

--- Personal_Mantine-Vite-master/CS411Project/Server/app.js
+++ Personal_Mantine-Vite-master/CS411Project/Server/app.js
@@ -1,9 +1,8 @@
 const express     = require("express")
 const app         = express()
-
+const connnection = require("./database/db")
 const cors        = require("cors")
-
-const routes = require("./routes/routes")
+const routes      = require("./routes/routes")
 
 require("dotenv").config()
 const port = process.env.PORT || 8081
@@ -12,6 +11,8 @@ app.use(express.json());
 app.use(cors());
 
 app.use('/api', routes)
+
+connnection();
 
 app.get('/', (req, res) => {
     res.send("hello world! Kashing out");
