068e1de41d5f15245e3c95a281426b55f38cdb87
Arkash Jain <arkjain@bu.edu>
2022-11-15 22:03:18 -0500

    finishing up views

--- Personal_Mantine-Vite-master/CS411Project/Server/Youtube/youtubeAPI.js
+++ Personal_Mantine-Vite-master/CS411Project/Server/Youtube/youtubeAPI.js
@@ -2,7 +2,7 @@
 
 router.get('/playlist', async(req, res) => {
     try {
-        url = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=Stay%20%by%20Justin%20Beiber&key=AIzaSyAWaNEFxGZYD1p33C6T_tlNvq4Jxs8Np2g"
+        url = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=Stay%20%by%20Justin%20Beiber&key=KEY"
     } catch (error){
         res.status(500).json({message: error.message})
     }
@@ -10,11 +10,14 @@ router.get('/playlist', async(req, res) => {
 
 // code to make playlist
 
-url = "https://youtube.googleapis.com/youtube/v3/playlists?part=snippet&key=AIzaSyAWaNEFxGZYD1p33C6T_tlNvq4Jxs8Np2g"
+url = "https://youtube.googleapis.com/youtube/v3/playlists?part=snippet&key=KEY"
 
 // when user selects songs, first, make a new playlist called your playlist. 
 // user needs oauth. Once done, Send the name and artist to the song url. It
 // will find a url and post the song to it
 
 
-url2 =  'https://youtube.googleapis.com/youtube/v3/playlists?part=snippet&key=[YOUR_API_KEY]' 
+url2 =  'https://youtube.googleapis.com/youtube/v3/playlists?part=snippet&key=[YOUR_API_KEY]'
+
+
+
