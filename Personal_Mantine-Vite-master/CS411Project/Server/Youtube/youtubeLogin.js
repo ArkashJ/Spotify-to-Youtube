d077a23152401c84abe6351571351fc7c6dc5283
Arkash Jain <arkjain@bu.edu>
2022-11-29 22:13:07 -0500

    setting scopes, discovery docs so that I can search songs and update them

--- Personal_Mantine-Vite-master/CS411Project/Server/Youtube/youtubeLogin.js
+++ Personal_Mantine-Vite-master/CS411Project/Server/Youtube/youtubeLogin.js
@@ -1,4 +1,20 @@
-var GoogleAuth;
-function initClient() {
+const YOUTUBE_Data = require("./data")
 
+var GoogleAuth;
+
+const scopes = [
+    'https://www.googleapis.com/auth/youtube.upload',
+    'https://www.googleapis.com/auth/youtube.force-ssl',
+    'https://www.googleapis.com/auth/youtube',
+    'https://www.googleapis.com/auth/youtubepartner',
+    'https://www.googleapis.com/auth/youtube.upload'
+]
+
+function initClient() {
+    gapi.client.init({
+        'apiKey'        : YOUTUBE_Data.API,
+        'clientId'      : YOUTUBE_Data.clientId,
+        'scope'         : scopes, 
+        'discoveryDocs' : ['https://www.googleapis.com/youtube/v3/playlists', 'https://www.googleapis.com/youtube/v3/search' ]
+    })
 }
