73162ab90fbacd1c0771a7198c3b127bf2d5b63d
Arkash Jain <arkjain@bu.edu>
2022-12-01 02:03:47 -0500

    search works...hours of frustration validated by at least some progress

---
+++ Personal_Mantine-Vite-master/CS411Project/Server/routes/playlists.js
@@ -0,0 +1,31 @@
+const express = require("express")
+const playlists = express.Router()
+const {google} = require("googleapis")
+const axios = require("axios")
+
+const baseUrl = "https://www.googleapis.com/youtube/v3"
+const apiKey  = "AIzaSyCNTwNQTDIF3nIbdQBW53rTFrenOV0vWM0"
+
+const youtube = google.youtube({
+    version: "v3",
+    auth:  apiKey
+})
+
+
+playlists.get('/search', async (req, res) => {
+    try{
+        const url = `${baseUrl}/search?key=${apiKey}&type=video&part=snippet&q=LilNasXHoliday`
+        const response = await youtube.search.list({
+            part: "snippet",
+            q: "LilNasXHoliday",
+            type: "video"
+        })
+        const titles = response.data.items.map((item) => item.snippet.title)
+        res.send(titles)
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+module.exports = playlists;
+
