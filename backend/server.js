6963f4535911f0ad6ea586ed1d1476f57c378ebb
Nada <nawahab@bu.edu>
2022-12-02 10:11:37 -0500

    started search ridirect page

--- backend/server.js
+++ backend/server.js
@@ -6,9 +6,18 @@ const { response } = require('express');
 const app = express();
 const port = 8800;
 
+// this is the search rideirect link
+// https://www.googleapis.com/youtube/v3/search?key=AIzaSyDgPCmpEvEZBkeEqVvliN0g_ZkqRWzfe4c&type=video&part=snippet&q=queryname
+
 app.get("/", (request, response) => {
   response.send("testing home page");
 });
+
+app.get("/search", (request, response) => {
+  const searchQuery = request.query.search_query;
+  // test sending back the request
+  response.send(searchQuery);
+})
 
 app.listen(port, () => {
   console.log("app started successfully");
