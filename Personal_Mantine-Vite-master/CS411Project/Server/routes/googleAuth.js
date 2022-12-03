e475d4e2c7dd13dc6b0745ae90365070b3f8d669
Arkash Jain <arkjain@bu.edu>
2022-12-02 19:31:44 -0500

    done

--- Personal_Mantine-Vite-master/CS411Project/Server/routes/googleAuth.js
+++ Personal_Mantine-Vite-master/CS411Project/Server/routes/googleAuth.js
@@ -1,16 +1,20 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const passport = require("passport");
+const {google} = require('googleapis');
 
-const router = express.Router();
+const oauth2Client = new google.auth.OAuth2(
+  "489746260370-afitsmlclo3gpg0vt58dmpfjf8koviaq.apps.googleusercontent.com",
+  "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",
+  "http://localhost:8081"
+);
 
-router.get("/google", passport.authenticate("google", {
-    scope: ["profile", "email"],
-}));
+// generate a url that asks permissions for Blogger and Google Calendar scopes
+const scopes = [
+  'https://www.googleapis.com/auth/youtube'
+];
 
-router.get("/google/redirect", passport.authenticate('google'), (req,res,next)=>{
-    user = req.user 
-    res.send(user)
-})
+const url = oauth2Client.generateAuthUrl({
+  // 'online' (default) or 'offline' (gets refresh_token)
+  access_type: 'offline',
 
-module.exports = router;
+  // If you only need one scope you can pass it as a string
+  scope: scopes
+});
