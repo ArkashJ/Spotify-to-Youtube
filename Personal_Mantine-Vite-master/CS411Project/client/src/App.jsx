e421eae1c619182631d96406d4e2f2788351ee11
Arkash Jain <arkjain@bu.edu>
2022-12-06 17:50:21 -0500

    fixing bugs

--- Personal_Mantine-Vite-master/CS411Project/client/src/App.jsx
+++ Personal_Mantine-Vite-master/CS411Project/client/src/App.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-import Login from "./components/LoginPage/Login";
 import MainPage from "./components/MainPage/MainPage";
-import DisplaySongs from "./components/DisplaySongs/displaySongs";
-import SignUp from "./components/LoginPage/Login";
+import DisplaySongs from "./components/MainPage/DisplaySongs";
 
 function App() {
 
   return (
     <div className="App" id="mainBody">
-      <MainPage/>
+      
+       <DisplaySongs/>
+      
     </div>
   )
 }
