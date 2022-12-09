78b7d47d00b7251fb32e860b5f35b35526d55d09
Arkash Jain <arkjain@bu.edu>
2022-12-09 09:48:42 -0500

    making it look good

--- Personal_Mantine-Vite-master/CS411Project/client/src/App.jsx
+++ Personal_Mantine-Vite-master/CS411Project/client/src/App.jsx
@@ -6,6 +6,7 @@ function App() {
 
   return (
     <div className="App" id="mainBody">
+      <MainPage/>
        <DisplaySongs/>
     </div>
   )
--- Personal_Mantine-Vite-master/CS411Project/client/src/components/MainPage/DisplaySongs.jsx
+++ Personal_Mantine-Vite-master/CS411Project/client/src/components/MainPage/DisplaySongs.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import React from "react";
 import {DataGrid} from '@mui/x-data-grid';
 import { Button } from '@mui/material';
+import { Grid } from "@mui/material";
 
 const baseURL = "http://localhost:8081/api/getAll";
 
@@ -16,7 +17,8 @@ const columns = [
 export default function DisplaySongs() {
   const [post, setPost] = React.useState(null);
   const [selectionModel, setSelectionModel] = React.useState([]);
-
+  
+  
   React.useEffect(() => {
     axios.get(baseURL).then((response) => {
       setPost(response.data);
@@ -52,26 +54,38 @@ export default function DisplaySongs() {
     // console.log('hello 3');
   }
 
+
   return (
     <div>
-      <div style={{ height: 800, width: '100%' }}>
-        <DataGrid
-          rows={rows}
-          columns={columns}
-          pageSize={10}
-          rowsPerPageOptions={[10]}
-          checkboxSelection
-          onSelectionModelChange={(newSelection) => {
-            setSelectionModel(newSelection.selectionModel);
-          }}
-          selectionModel={selectionModel}
-          getRowId={(row) => row.key}
-        />
-      </div>
-      {selectionModel && selectionModel.map((val) => (
-        <h1>{val}</h1>
-      ))}
-      <Button variant="contained" onClick={sendToYoutube}>Transfer</Button>
+      <Grid
+        container
+        
+        top={100}
+        direction="column"
+        alignItems="center"
+        justify="center"
+      >
+        <Grid item xs = {3}>
+        <div style={{ height: 700, width: '400%', marginTop: 100}}>
+          <DataGrid
+            rows={rows}
+            columns={columns}
+            pageSize={30}
+            rowsPerPageOptions={[10]}
+            checkboxSelection
+            onSelectionModelChange={(newSelection) => {
+              setSelectionModel(newSelection.selectionModel);
+            }}
+            selectionModel={selectionModel}
+            getRowId={(row) => row.key}
+          />
+        </div>
+        {selectionModel && selectionModel.map((val) => (
+          <h1>{val}</h1>
+        ))}
+        <Button variant="contained" onClick={sendToYoutube}>Transfer</Button>
+        </Grid>
+        </Grid>
     </div>
     
   );
